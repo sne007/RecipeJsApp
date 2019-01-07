30a43f194d3080ec10b8d060ae97ef0322ac25ca
Snehith Challa <challasnehith@gmail.com>
2019-01-06 23:28:28 -0500

    import all the modules

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Global app controller
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import List from './models/List';
+import Likes from './models/Likes';
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import * as likesView from './views/likesView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
